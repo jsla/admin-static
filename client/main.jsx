4633546e97f67ed61c03e0e43653f940a1a13041
David Guttman <david@davidguttman.com>
2017-10-11 15:41:16 -0700

    feat: nav drawer

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
 import Button from 'material-ui/Button'
+import Divider from 'material-ui/Divider'
 import Toolbar from 'material-ui/Toolbar'
+import MenuIcon from 'material-ui-icons/Menu'
+import IconButton from 'material-ui/IconButton'
 import Typography from 'material-ui/Typography'
+import ChevronLeftIcon from 'material-ui-icons/ChevronLeft'
+import List, { ListItem, ListItemText } from 'material-ui/List'
 import createReactClass from 'create-react-class'
 
 import auth from './auth'
@@ -44,6 +50,43 @@ module.exports = createReactClass({
     return (
       <div>
         {this.renderHeader()}
+        <Drawer
+          type='persistent'
+          open={this.state.open}
+        >
+          <div>
+            <div>
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+          </div>
+          <Divider />
+          <List>
+            <ListItem
+              button
+              component='a'
+              href='#/hosts'
+              onClick={this.handleDrawerClose}>
+              <ListItemText primary='Hosts' />
+            </ListItem>
+            <ListItem
+              button
+              component='a'
+              href='#/speakers'
+              onClick={this.handleDrawerClose}>
+              <ListItemText primary='Speakers' />
+            </ListItem>
+            <ListItem
+              button
+              component='a'
+              href='#/sponsors'
+              onClick={this.handleDrawerClose}>
+              <ListItemText primary='Sponsors' />
+            </ListItem>
+          </List>
+          <Divider />
+        </Drawer>
         <div>{main}</div>
       </div>
     )
@@ -57,13 +100,27 @@ module.exports = createReactClass({
     return (
       <AppBar position='static'>
         <Toolbar>
+          <IconButton
+            color='contrast'
+            aria-label='open drawer'
+            onClick={this.handleDrawerOpen} >
+            <MenuIcon />
+          </IconButton>
           <Typography type='title' color='inherit' style={{flex: 1}}>
-            js.la Admin
+            <Button color='contrast' href='#/'>js.la Admin</Button>
           </Typography>
           { logout }
         </Toolbar>
       </AppBar>
     )
+  },
+
+  handleDrawerOpen () {
+    this.setState({ open: true })
+  },
+
+  handleDrawerClose () {
+    this.setState({ open: false })
   },
 
   onRoute () {
